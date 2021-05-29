e678b654454a55b1f2d186ef4de22f07c2a6bf69
Mattia Gheda <ghedamat@gmail.com>
2021-05-29 06:01:25 +0000

    basic results api

--- lib/conta/router.ex
+++ lib/conta/router.ex
@@ -3,6 +3,26 @@ defmodule Conta.Router do
   plug(:match)
   plug(:dispatch)
 
+  get "/stats/:domain" do
+    {:ok, res} = Conta.Redis.command(["KEYS", "VISITS:/#{domain}/*"])
+
+    val =
+      Enum.map(res, fn e ->
+        {:ok, res} = Conta.Redis.command(["HGETALL", e])
+
+        map =
+          res
+          |> Enum.chunk_every(2)
+          |> Enum.map(fn [a, b] -> {a, b} end)
+          |> Map.new()
+
+        %{e => map}
+      end)
+
+    conn
+    |> send_resp(200, Jason.encode!(val))
+  end
+
   match _ do
     # TODO cache image payload in memory
     app_dir = Application.app_dir(:conta, "priv")
@@ -13,7 +33,6 @@ defmodule Conta.Router do
     set = "VISITS:#{path}"
     key = Date.utc_today() |> Date.to_string()
 
-    IO.inspect(set)
     Conta.Redis.command(["HINCRBY", set, key, 1])
 
     conn
